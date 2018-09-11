fe2a33bf2b75758bccaf077aee1c8197b0424076
Josh Womack <womack.sjosh@gmail.com>
2018-09-10 21:52:34 -0500

    added comments

--- app.js
+++ app.js
@@ -2,27 +2,43 @@
 var friends = ['Bill','Bob','Ted','Sue','Jill'];
 var locations = ['hallway','upstairs bathroom','downstairs bathroom','pantry','kitchen','dining room','master bedroom','guest bedroom','cellar','attic']
 var weapons = ['knife','baseball bat','JavaScript videos','syntax errors','lack of sleep','gun','shoe','request to work all weekend','paper airplane','board','hammer','lead pipe','rock','ping pong ball','nacho cheese dorito','rice maker','pencil','scizzors','high blood pressure','lack of exercise'];
-var displayAlert;
+
 
 for (var i = 0; i < 100; i++) {
-    var clue = document.createElement('h3');
+    var accusation = document.createElement('h3');
+    accusation.className = 'accusation';
 
-    clue.textContent = `Accusation ${i + 1}`
-    document.body.appendChild(clue);
+    accusation.textContent = `Accusation ${i + 1}`
+    document.body.appendChild(accusation);
 
+
+    // This works
     function createFunction() {
-        var clueNum = i + 1;
+        var accusationNum = i + 1;
         var friend = friends[i % 5];
         var location = locations[i % 10];
         var weapon = weapons[i % 20];
-        return function() {
-            alert(`Accusation ${clueNum}: I accuse ${friend}, with the ${weapon}, in the ${location}!`);
+        return () => {
+            alert(`Accusation ${accusationNum}: I accuse ${friend}, with the ${weapon}, in the ${location}!`);
         }
     }
 
-    displayAlert = createFunction();
+    var displayAlert = createFunction();
 
-    clue.addEventListener('click',displayAlert);
+    // I'm not understanding why this does not work
+    // var displayAlert = () => {
+    //     var accusationNum = i + 1;
+    //     var friend = friends[i % 5];
+    //     var location = locations[i % 10];
+    //     var weapon = weapons[i % 20];
+    //     return () => {
+    //         alert(`Accusation ${accusationNum}: I accuse ${friend}, with the ${weapon}, in the ${location}!`);
+    //     }
+    // }
+
+
+
+    accusation.addEventListener('click',displayAlert);
 }
 
 
